d44c91375b9426405dbf67d154b9bb4b6e246de0
Alx-Assis <alxassis@gmail.com>
2022-08-13 07:12:06 -0300

    ...

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const employees = require('./src/controller/employees');
 const cors = require('cors');
+const employees = require('./src/controller/employees');
 
 const server = express();
 const port = 8080;
 
-server.use(express.json());
+server.use(express.json({ extended: false }));
 server.use(cors());
 
 server.post('/employee', employees.create);
@@ -15,8 +15,6 @@ server.get('/employee_cpf', employees.readEmployeeCpf);
 server.put('/employee', employees.updateEmployee);
 server.delete('/employee', employees.deleteEmployee);
 
-
-
-server.listen(3333, () => {
+server.listen(port, () => {
   console.log(`servidor online na porta ${port}`)
 });
